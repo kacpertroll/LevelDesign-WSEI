2122cfe6288b495549661212b59ec7e37f13c338
Kacper Żołna <kacper.zolna@microsoft.wsei.edu.pl>
2026-05-09 13:21:55 +0200

    blockout finish

---
+++ Assets/Scripts/InventoryController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour
+{
+    public bool canInteract = false;
+    public float interactDistance = 3f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
